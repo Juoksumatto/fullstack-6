08867e0c0ec7f30c97c8b1aa215ec0592aece40c
onni <holttaonni@gmail.com>
2026-05-28 08:37:12 +0300

    Add files via upload

---
+++ 6.5 tehtävä/zustand-anecdotes-main/src/components/AnecdoteList.jsx
@@ -0,0 +1,26 @@
+import { useAnecdoteStore } from '../store'
+
+const AnecdoteList = () => {
+    const anecdotes = useAnecdoteStore((state) => state.anecdotes)
+    const vote = useAnecdoteStore((state) => state.actions.vote)
+
+    return (
+        <div>
+            {anecdotes.map(anecdote =>
+                <div key={anecdote.id}>
+                    <div>
+                        {anecdote.content}
+                    </div>
+                    <div>
+                        has {anecdote.votes}
+                        <button onClick={() => vote(anecdote.id)}>
+                            vote
+                        </button>
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default AnecdoteList
